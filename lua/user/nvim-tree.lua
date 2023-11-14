c981bf8bb8dd652170f6015012d9b1fafb62f409
Corentin Chataignon <corentin.chataignon@kaduceo.com>
2023-11-14 14:55:22 +0100

    fix(nvim-tree): remove netrw

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,6 +1,8 @@
 local nvim_tree = require("nvim-tree")
 
 nvim_tree.setup({
+	disable_netrw = true,
+	hijack_netrw = true,
 	actions = {
 		open_file = {
 			quit_on_open = true,
@@ -9,6 +11,5 @@ nvim_tree.setup({
 	update_focused_file = {
 		enable = true,
 		update_root = false,
-		ignore_list = {},
 	},
 })
